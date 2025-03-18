6d3b71b854a19b4276c952cf1d5a56bec6823cc3
BudliCode <birger@luland.de>
2025-03-18 09:26:40 +0100

    zsh stuff

--- dot-config/zsh/exports.zsh
+++ dot-config/zsh/exports.zsh
@@ -16,6 +16,7 @@ export EDITOR="nvim"
 export BAT_THEME="Catppuccin Mocha"
 
 export PATH="$HOME/.local/bin":$PATH
+export PATH="$HOME/go/bin":$PATH
 
 fzf_mocha="--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8,fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc,marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 fzf_prompt="-m --prompt='▶' --pointer='→' --marker='♡'"
